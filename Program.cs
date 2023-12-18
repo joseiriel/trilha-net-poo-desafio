1886fffac5b08803af7fabe54cac9230ac37b47a
José Iriel de Freitas Martins <jose.iriel.martins@gmail.com>
2023-12-18 16:11:19 -0300

    Realiza o desafio proposto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "91234-5678", modelo: "Lumia 1320", imei: "123456789", memoria: 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Signal");
+
+Console.WriteLine();
+
+Console.WriteLine("iPhone:");
+Smartphone iphone = new Iphone(numero: "98765-4321", modelo: "iPhone SE", imei: "987654321", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Wild Rift");
